6ed1bcb1ca4ec8ec556456eac1d123dbcd2567cb
camille <camillermpoirier@gmail.com>
2023-12-05 16:51:11 +0100

    clean retour code

--- retour/src/main/java/fr/alom/retour/front/ServeurTCP.java
+++ retour/src/main/java/fr/alom/retour/front/ServeurTCP.java
@@ -20,11 +20,6 @@ public class ServeurTCP {
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Connecté au serveur " + serverAddress + ":" + port);
 
-            //BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //PrintStream output = new PrintStream(socket.getOutputStream());
-
-            //Scanner scanner = new Scanner(System.in);
-
             while (isRunnable) {
                 // Accepter une connexion entrante
                 Socket clientSocket = serverSocket.accept();
@@ -38,20 +33,6 @@ public class ServeurTCP {
 
                 connexionClient.start(); // Démarrer le traitement de la connexion dans un nouveau thread
             }
-
-            // Demande au client d'entrer son token
-            //System.out.println(input.readLine()); // Attend le message du serveur demandant le token
-            //String token = scanner.nextLine();
-            //output.println(token); // Envoie le token au serveur
-
-            // Lecture du nickname associé au token envoyé
-            //String serverResponse = input.readLine();
-            //System.out.println("Réponse du serveur : " + serverResponse);
-
-            //Appel au Microservice de gestion de token/nickname
-            //String nickname = "nickanme1";
-
-
             
         } catch (IOException e) {
             e.printStackTrace();
